807074a3979963146cc9c496069f130e7f230369
Misha-Mody <mishuu.mmm@gmail.com>
2022-12-12 00:06:32 -0800

    handle other routes in express

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front2/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "front2/build/index.html"));
+});
 
 app.use("/", indexRouter);
 
